6feb20a11c7ff13c2c4ddfcc2dae03022e0e026a
RonnieNigash <nigash@uw.edu>
2017-03-16 10:39:16 -0700

    Add intensity and test

--- tests/RenderTest.cpp
+++ tests/RenderTest.cpp
@@ -2,7 +2,7 @@
 #include "CppUTest/TestHarness.h"
 
 extern "C" {
-#include "objects.h"
+#include "radiance.h"
 }
 
 #define TOL 0.0001 // Tolerance
@@ -10,16 +10,29 @@ extern "C" {
 TEST_GROUP(RenderTestGroup)
 {
 	// run before each test group
+	Ray *testRay;
+	Vector *vecA;
+	Vector *vecB;
 	void setup()
 	{
+		vecA = initVector( 0, 0, 0 );
+		vecB = initVector( 1, 1, 1 );
+		testRay = initRay( vecA, vecB );
 	}
 
-	// run after each test group
 	void teardown()
 	{
+		free(vecA);
+		free(vecB);
+		free(testRay);
 	}
 };
 
 TEST(RenderTestGroup, InitTest)
 {
+	Vector *intensityVec = intensity(testRay, 1, 1);
+	DOUBLES_EQUAL(intensityVec->x,0,TOL);
+	DOUBLES_EQUAL(intensityVec->y,0,TOL);
+	DOUBLES_EQUAL(intensityVec->z,0,TOL);
+	free(intensityVec);
 }
