42da37287e441870cc602af50724f959214f8041
RonnieNigash <nigash.ronnie@gmail.com>
2017-03-23 08:52:59 -0700

    Initialize camera and image array

--- src/main.c
+++ src/main.c
@@ -1,11 +1,25 @@
 #include <stdlib.h>
 
+#include "objects.h"
+#include "intensity.h"
 
 int main( int argc, char *argv[] ) {
-	// place camera
-	
+
+	// initialize camera
+	Vector *position = initVector(50, 50, 300);
+	Vector *direction= initVector(0, 0, -1);
+	Ray *camera = initRay(position, direction);
+
 	// initialize array for the image
-	
+	int width = 1680, height = 1050;
+
+	int samples = 8; 
+
+	if (argc == 2) {
+		samples = atoi(argv[1])/4;
+	}	
+
+
 	// for each pixel
 	// 	split into 2x2 sub pixels
 	// 	average the intensity samples
